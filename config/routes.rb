85f22c5151ab98097bea08e468d9c3b2af43cbb7
Matt Tarbit <mtarbit@gmail.com>
2011-12-13 16:29:22 +0000

    Added, removed & edited files and dirs to match basic structure of a fresh rails 3.1 project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,74 +1,87 @@
-ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
+MattTarbitOrg::Application.routes.draw do
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
   # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
+  #   resources :products
 
   # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
 
   # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
   # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
   #   end
 
   # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
-  map.index     '', :controller=>'blog'
-  map.about     'about', :controller=>'blog', :action=>'about'
-  map.archive   'archive', :controller=>'blog', :action=>'archive'
-  map.titles    'archive/titles', :controller=>'blog', :action=>'archives_by_title'
-  map.search    'search', :controller=>'blog', :action=>'search'
-  map.tags	    'tag', :controller=>'tag'
-  map.tag       'tag/:name', :controller=>'tag', :action=>'read'
-  map.rss       'rss', :controller=>'feed', :action=>'rss'
+  root          :to => 'blog#index', :as => :index
 
-  map.admin		  'admin', :controller=>'admin'
-  map.comments	'admin/comments', :controller=>'comment'
-  map.librarian	'admin/library', :controller=>'admin', :action=>'library'
-  map.library	  'book', :controller=>'book'
-  map.logout    'logout', :controller=>'admin', :action=>'logout'
-  map.new_book  'new/book', :controller=>'book', :action=>'new'
-  map.new_entry 'new/:variant', :controller=>'entry', :action=>'new'
+  match         'about' => 'blog#about', :as => :about
+  match         'archive' => 'blog#archive', :as => :archive
+  match         'archive/titles' => 'blog#archives_by_title', :as => :titles
+  match         'search' => 'blog#search', :as => :search
+  match         'tag' => 'tag#index', :as => :tags
+  match         'tag/:name' => 'tag#read', :as => :tag
+  match         'rss' => 'feed#rss', :as => :rss
 
-  map.connect   'page/:page', :controller=>'blog'
-  map.connect 	'hobby_games', :controller=>'blog', :action=>'hobby_games'
-  map.connect 	'family_games', :controller=>'blog', :action=>'family_games'
-  map.cv        'cv', :controller=>'blog', :action=>'cv'
+  match         'admin' => 'admin#index', :as => :admin
+  match         'admin/comments' => 'comment#index', :as => :comments
+  match         'admin/library' => 'admin#library', :as => :librarian
+  match         'book' => 'book#index', :as => :library
+  match         'logout' => 'admin#logout', :as => :logout
+  match         'new/book' => 'book#new', :as => :new_book
+  match         'new/:variant' => 'entry#new', :as => :new_entry
 
-  map.date ':y/:m/:d', 
-    :controller=>'blog', :action=>'date', :m=>nil, :d=>nil,
-    :requirements => { :y=>/(19|20)\d\d/, :m=>/[01]?\d/, :d=>/[0-3]?\d/ }
+  match         'hobby[-_]games' => 'blog#hobby_games'
+  match         'family[-_]games' => 'blog#family_games'
+  match         'cv' => 'blog#cv'
 
-  map.entry_slug ':y/:m/:d/:slug', 
-    :controller=>'entry', :action=>'read',
-    :requirements => { :y=>/(19|20)\d\d/, :m=>/[01]?\d/, :d=>/[0-3]?\d/, :slug=>/[a-z0-9._-]+/ }
+  match ':y(/:m(/:d))' => 'blog#date', :as => :date, 
+    :constraints => { :y=>/(19|20)\d\d/, :m=>/[01]?\d/, :d=>/[0-3]?\d/ }
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
+  match ':y/:m/:d/:slug' => 'entry#read', :as => :entry_slug,
+    :constraints => { :y=>/(19|20)\d\d/, :m=>/[01]?\d/, :d=>/[0-3]?\d/, :slug=>/[a-z0-9._-]+/ }
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  match ':controller(/:action(/:id(.:format)))'
 
 end
