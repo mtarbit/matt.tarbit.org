8bc18104a0365d76b81a53b40a7566173dfba1eb
Matt Tarbit <mtarbit@gmail.com>
2011-12-20 14:18:48 +0000

    Migrating JS from Prototype to jQuery (first pass, may still be more to do).

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -1,103 +1,57 @@
-function pick(set) {
-    var i = Math.round((set.length - 1) * Math.random());
-    return set[i];
-}
-
-function initHomepageHeaders(){
-    var homepage = $('blog-index');
-    if (!homepage) return;
-
-    var headerA = homepage.down('h2#long-entries');
-    var headerB = homepage.down('h2#short-entries');
-    if (headerA && headerB) {
-        var strings = pick([
-            ['Verbosity', 'Concision'],
-            ['Long', 'Short'],
-            ['Long &amp; Straggly', 'Short &amp; Curly'],
-            ['Long &amp; Winding', 'Short &amp; Sweet'],
-            ['Paragraphs', 'Sentences'],
-            ['Diatribes', 'Epithets'],
-            ['Here', 'There'],
-            ['Hither', 'Thither'],
-            ['Home', 'Away'],
-            ['This', 'That'],
-            ['Tomes', 'Pamphlets'],
-            ['Slow &amp; Laborious', 'Quick &amp; Easy'],
-            ['Rarely', 'Often'],
-            ['Banquets', 'Snacks'],
-            ['Weblog', 'Linklog']
-        ]);
-        headerA.update(strings[0]);
-        headerB.update(strings[1]);
-    }
-}
-
 function initSlugField() {
-    var fieldA = $('entry_title');
-    var fieldB = $('entry_slug');
-    if (fieldA && fieldB) {
-        function generateSlug(){
-            var val = fieldA.value.toLowerCase();
-            val = val.replace(/i've been [^:]*: /,''); // Reviews
-            val = val.replace(/[^a-z0-9._ ]+/g,'').replace(/ +/g,'-');
-            val = val.split('-').slice(0,3).join('-');
-            fieldB.value = val;
-        }
-        fieldA.observe('keyup',generateSlug);
-        generateSlug();
-    }
+    var a = $('#entry_title');
+    var b = $('#entry_slug');
+    a.keyup(function(){
+        var val = a.val().toLowerCase();
+        val = val.replace(/i've been [^:]*: /,''); // Reviews
+        val = val.replace(/[^a-z0-9._ ]+/g,'').replace(/ +/g,'-');
+        val = val.split('-').slice(0,3).join('-');
+        b.val(val);
+    }).keyup();
 }
 
 function initCodeLineNumbers() {
-    $$('pre.code code').each(function(code){
-        var nodes = code.childNodes;
+    $('pre.code').each(function(code){
+        var nodes = $(this).find('code').get(0).childNodes;
         var count = 1;
         var lines = [];
+
         for (var i=0; i<nodes.length; i++) {
             if (nodes[i].nodeType != 3) continue;
             var matches = nodes[i].nodeValue.match(/[\r\n]/g);
             if (matches) count += matches.length;
         }
-        for (var i=1; i<count; i++) { lines.push(i); }
-        code.up().insert({before:'<pre class="line"><code>'+lines.join("\n")+'</code></pre>'});
+
+        for (var i=1; i<count; i++) {
+            lines.push(i);
+        }
+
+        $(this).prepend('<pre class="line"><code>'+lines.join("\n")+'</code></pre>');
     });
 }
 
 function initVideoLink() {
-    $$('body#entry-read .video-link').each(function(link){
-        link.observe('click', function(e){
-            if (e) e.stop();
-            var div = $$('.video-player').first();
-            if (div) div.show();
-        });
+    $('body#entry-read .video-link').click(function(e){
+        $('.video-player').show();
+        e.preventDefault();
     });
 }
 
-Element.addMethods({
-    visible: function(element) {
-        return $(element).getStyle('display') != 'none';
-    },
-    show: function(element){
-        $(element).setStyle({ display: $(element).oldblock || '' });
-        if ($(element).getStyle('display') == 'none') {
-            var newElement = document.createElement($(element).tagName);
-            $$('body').first().insert({bottom: newElement});
-            $(element).setStyle({ display: $(newElement).getStyle('display')});
-            $(newElement).remove();
-            if ($(element).getStyle('display') == 'none') $(element).setStyle({ display: 'block'});
+function initImages() {
+    var re = /_t\.jpg$/;
+    $('div.img a').each(function(){
+        var link = $(this);
+        var img = link.find('img');
+        var src = img.attr('src');
+        if (src && re.test(src)) {
+            link.attr('href', src.replace(re, '.jpg'));
+            link.click(function(e){ window.open(this.href); e.preventDefault(); });
         }
-        return element;
-    },
-    hide: function(element){
-        $(element).oldblock = $(element).oldblock || $(element).getStyle("display");
-        $(element).setStyle({display: 'none'});
-        return element;
-    }
-});
+    })
+}
 
-document.observe('dom:loaded',function(){
+$(function(){
     initCodeLineNumbers();
-    initHomepageHeaders();
     initSlugField();
     initVideoLink();
 });
