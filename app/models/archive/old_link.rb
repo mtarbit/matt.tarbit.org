0aeb40b117193a553bdff2838b77c09d8c2b3b7e
Matt Tarbit <mtarbit@gmail.com>
2012-01-04 21:59:07 +0000

    Removed archived legacy models.

--- app/models/archive/old_link.rb
+++
@@ -1,7 +0,0 @@
-class OldLink < ActiveRecord::Base
-  establish_connection(:legacy)
-  set_table_name "links"
-  has_and_belongs_to_many :old_tags, :join_table=>'links_tags', 
-    :association_foreign_key=>'tag_id',
-    :foreign_key=>'link_id'
-end
--- app/models/archive/old_user.rb
+++
@@ -1,5 +0,0 @@
-class OldUser < ActiveRecord::Base
-  establish_connection(:legacy)
-  set_table_name "users"
-  has_many :old_comments, :foreign_key => 'user_id'
-end
